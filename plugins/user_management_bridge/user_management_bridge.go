6019ad3a5481ccfea0c3383621b8dc25f1e94c19
Tim Whitbeck <tim.whitbeck@stord.com>
2022-04-21 14:59:02 -0400

    Skip user management bridge for finops paths (#179)

--- plugins/user_management_bridge/user_management_bridge.go
+++ plugins/user_management_bridge/user_management_bridge.go
@@ -92,6 +92,11 @@ func (conf Config) Access(kong *pdk.PDK) {
 		return
 	}
 
+	match, _ = regexp.MatchString("/finops", path)
+	if match {
+		return
+	}
+
 	auth0Token, err := getAuth0Token(kong)
 	if err != nil {
 		kong.Log.Warn("warning: ", err.Error())
