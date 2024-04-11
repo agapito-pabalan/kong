dde3d28ae18ef7bc333a77997856db46f585cba7
Tim Whitbeck <tim.whitbeck@stord.com>
2024-04-11 12:15:48 -0400

    fix: [OMS-2662] bypass auth for new image endpoint (#425)

--- plugins/user_management_bridge/user_management_bridge.go
+++ plugins/user_management_bridge/user_management_bridge.go
@@ -169,6 +169,11 @@ func (conf *Config) Access(kong *pdk.PDK) {
 		return
 	}
 
+	match, _ = regexp.MatchString("/networks/.+/items/.+/image", path)
+	if match {
+		return
+	}
+
 	cloudSignatureHeader, err := kong.Request.GetHeader(CLOUD_SIGNATURE_HEADER)
 	if err != nil {
 		kong.Log.Err("error: ", err.Error(), " unable to read header")
