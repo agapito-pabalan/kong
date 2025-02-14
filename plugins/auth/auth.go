07fcedfd676e8a20993075c414bcac027553bfa6
Tim Whitbeck <tim.whitbeck@stord.com>
2025-02-14 16:31:39 -0500

    fix: add new endpoint to skip cloud auth (#512)
    The new endpoint in marketplace to fetch a shopify connection by nonce
    should allow non-admins.

--- plugins/auth/auth.go
+++ plugins/auth/auth.go
@@ -119,6 +119,7 @@ func (conf *Config) Access(kong *pdk.PDK) {
 		"/public_portal/",
 		"/networks/.+/items/.+/image",
 		"/shopify_extensions/v2",
+		"/v1/connections/shopify/by_nonce",
 	}
 
 	for _, pathRegex := range skipAuthPaths {
